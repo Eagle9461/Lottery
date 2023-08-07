f51c36a5f328ff23162c4cc17cffe8456d56390b
Eagle9461 <Eagle9461@outlook.com>
2023-08-07 16:02:45 -0300

    Initialize control elements.

--- SelectLuckyNumber/SelectLuckyNumber/MainForm.Designer.cs
+++ SelectLuckyNumber/SelectLuckyNumber/MainForm.Designer.cs
@@ -28,43 +28,383 @@
         /// </summary>
         private void InitializeComponent()
         {
-            selGameType = new ComboBox();
+            tabControl1 = new TabControl();
+            tabDataEntry = new TabPage();
+            btnSave = new Button();
+            monthCalendar1 = new MonthCalendar();
+            label9 = new Label();
+            comboBox3 = new ComboBox();
+            label11 = new Label();
+            label10 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            numericUpDown6 = new NumericUpDown();
+            numericUpDown5 = new NumericUpDown();
+            numericUpDown4 = new NumericUpDown();
+            numericUpDown3 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            numericUpDown1 = new NumericUpDown();
+            numData1 = new NumericUpDown();
             label1 = new Label();
+            selGameType = new ComboBox();
+            tabCalculate = new TabPage();
+            listView1 = new ListView();
+            colDateTime = new ColumnHeader();
+            colNumbers = new ColumnHeader();
+            tabControl1.SuspendLayout();
+            tabDataEntry.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numData1).BeginInit();
+            tabCalculate.SuspendLayout();
             SuspendLayout();
             // 
-            // selGameType
+            // tabControl1
             // 
-            selGameType.FormattingEnabled = true;
-            selGameType.Location = new Point(93, 82);
-            selGameType.Name = "selGameType";
-            selGameType.Size = new Size(162, 23);
-            selGameType.TabIndex = 0;
+            tabControl1.Controls.Add(tabDataEntry);
+            tabControl1.Controls.Add(tabCalculate);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(865, 352);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabDataEntry
+            // 
+            tabDataEntry.Controls.Add(btnSave);
+            tabDataEntry.Controls.Add(monthCalendar1);
+            tabDataEntry.Controls.Add(label9);
+            tabDataEntry.Controls.Add(comboBox3);
+            tabDataEntry.Controls.Add(label11);
+            tabDataEntry.Controls.Add(label10);
+            tabDataEntry.Controls.Add(comboBox2);
+            tabDataEntry.Controls.Add(comboBox1);
+            tabDataEntry.Controls.Add(label8);
+            tabDataEntry.Controls.Add(label7);
+            tabDataEntry.Controls.Add(label6);
+            tabDataEntry.Controls.Add(label5);
+            tabDataEntry.Controls.Add(label4);
+            tabDataEntry.Controls.Add(label3);
+            tabDataEntry.Controls.Add(label2);
+            tabDataEntry.Controls.Add(numericUpDown6);
+            tabDataEntry.Controls.Add(numericUpDown5);
+            tabDataEntry.Controls.Add(numericUpDown4);
+            tabDataEntry.Controls.Add(numericUpDown3);
+            tabDataEntry.Controls.Add(numericUpDown2);
+            tabDataEntry.Controls.Add(numericUpDown1);
+            tabDataEntry.Controls.Add(numData1);
+            tabDataEntry.Controls.Add(label1);
+            tabDataEntry.Controls.Add(selGameType);
+            tabDataEntry.Location = new Point(4, 24);
+            tabDataEntry.Name = "tabDataEntry";
+            tabDataEntry.Padding = new Padding(3);
+            tabDataEntry.Size = new Size(857, 324);
+            tabDataEntry.TabIndex = 0;
+            tabDataEntry.Text = "Data Entry";
+            tabDataEntry.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(583, 269);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 33;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(583, 95);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 32;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(583, 33);
+            label9.Name = "label9";
+            label9.Size = new Size(86, 15);
+            label9.TabIndex = 31;
+            label9.Text = "Drawing Time :";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(583, 54);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(162, 23);
+            comboBox3.TabIndex = 30;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(228, 208);
+            label11.Name = "label11";
+            label11.Size = new Size(122, 15);
+            label11.TabIndex = 29;
+            label11.Text = "Extra Number Range :";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(47, 208);
+            label10.Name = "label10";
+            label10.Size = new Size(98, 15);
+            label10.TabIndex = 28;
+            label10.Text = "Numbers Range :";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(228, 229);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(162, 23);
+            comboBox2.TabIndex = 27;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(47, 229);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(162, 23);
+            comboBox1.TabIndex = 26;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(467, 95);
+            label8.Name = "label8";
+            label8.Size = new Size(39, 15);
+            label8.TabIndex = 24;
+            label8.Text = "Extra :";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(397, 95);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 15);
+            label7.TabIndex = 23;
+            label7.Text = "Num 6 :";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(327, 95);
+            label6.Name = "label6";
+            label6.Size = new Size(49, 15);
+            label6.TabIndex = 25;
+            label6.Text = "Num 5 :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(257, 95);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 15);
+            label5.TabIndex = 22;
+            label5.Text = "Num 4 :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(187, 95);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 15);
+            label4.TabIndex = 21;
+            label4.Text = "Num 3 :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(117, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 20;
+            label3.Text = "Num 2 :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(47, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 15);
+            label2.TabIndex = 19;
+            label2.Text = "Num 1 :";
+            // 
+            // numericUpDown6
+            // 
+            numericUpDown6.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            numericUpDown6.Location = new Point(466, 121);
+            numericUpDown6.Name = "numericUpDown6";
+            numericUpDown6.Size = new Size(64, 46);
+            numericUpDown6.TabIndex = 18;
+            numericUpDown6.TextAlign = HorizontalAlignment.Center;
+            // 
+            // numericUpDown5
+            // 
+            numericUpDown5.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            numericUpDown5.Location = new Point(396, 121);
+            numericUpDown5.Name = "numericUpDown5";
+            numericUpDown5.Size = new Size(64, 46);
+            numericUpDown5.TabIndex = 17;
+            numericUpDown5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            numericUpDown4.Location = new Point(326, 121);
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(64, 46);
+            numericUpDown4.TabIndex = 16;
+            numericUpDown4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            numericUpDown3.Location = new Point(256, 121);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(64, 46);
+            numericUpDown3.TabIndex = 15;
+            numericUpDown3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            numericUpDown2.Location = new Point(186, 121);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(64, 46);
+            numericUpDown2.TabIndex = 14;
+            numericUpDown2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            numericUpDown1.Location = new Point(116, 121);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(64, 46);
+            numericUpDown1.TabIndex = 13;
+            numericUpDown1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // numData1
+            // 
+            numData1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            numData1.Location = new Point(46, 121);
+            numData1.Name = "numData1";
+            numData1.Size = new Size(64, 46);
+            numData1.TabIndex = 12;
+            numData1.TextAlign = HorizontalAlignment.Center;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(93, 61);
+            label1.Location = new Point(47, 33);
             label1.Name = "label1";
-            label1.Size = new Size(62, 15);
-            label1.TabIndex = 1;
-            label1.Text = "GameType";
+            label1.Size = new Size(71, 15);
+            label1.TabIndex = 11;
+            label1.Text = "Game Type :";
+            // 
+            // selGameType
+            // 
+            selGameType.FormattingEnabled = true;
+            selGameType.Location = new Point(47, 54);
+            selGameType.Name = "selGameType";
+            selGameType.Size = new Size(162, 23);
+            selGameType.TabIndex = 10;
+            // 
+            // tabCalculate
+            // 
+            tabCalculate.Controls.Add(listView1);
+            tabCalculate.Location = new Point(4, 24);
+            tabCalculate.Name = "tabCalculate";
+            tabCalculate.Padding = new Padding(3);
+            tabCalculate.Size = new Size(857, 324);
+            tabCalculate.TabIndex = 1;
+            tabCalculate.Text = "Get Lucky";
+            tabCalculate.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { colDateTime, colNumbers });
+            listView1.Location = new Point(34, 22);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(210, 261);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // colDateTime
+            // 
+            colDateTime.Text = "DateTime";
+            // 
+            // colNumbers
+            // 
+            colNumbers.Text = "Numbers";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
-            Controls.Add(selGameType);
+            ClientSize = new Size(889, 376);
+            Controls.Add(tabControl1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
             Name = "MainForm";
             Text = "SelectLuckyNumber";
+            tabControl1.ResumeLayout(false);
+            tabDataEntry.ResumeLayout(false);
+            tabDataEntry.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numData1).EndInit();
+            tabCalculate.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private ComboBox selGameType;
+        private TabControl tabControl1;
+        private TabPage tabDataEntry;
+        private Button btnSave;
+        private MonthCalendar monthCalendar1;
+        private Label label9;
+        private ComboBox comboBox3;
+        private Label label11;
+        private Label label10;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private NumericUpDown numericUpDown6;
+        private NumericUpDown numericUpDown5;
+        private NumericUpDown numericUpDown4;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numData1;
         private Label label1;
+        private ComboBox selGameType;
+        private TabPage tabCalculate;
+        private ListView listView1;
+        private ColumnHeader colDateTime;
+        private ColumnHeader colNumbers;
     }
 }
