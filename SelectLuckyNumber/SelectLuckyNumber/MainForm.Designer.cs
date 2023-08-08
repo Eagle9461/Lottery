a377bfc3b0b012a2e496e00d3df0d19637c045ef
Eagle9461 <Eagle9461@outlook.com>
2023-08-08 04:23:03 -0300

    Add valid judgement in number entry.

--- SelectLuckyNumber/SelectLuckyNumber/MainForm.Designer.cs
+++ SelectLuckyNumber/SelectLuckyNumber/MainForm.Designer.cs
@@ -30,10 +30,12 @@
         {
             tabPane = new TabControl();
             tabDataEntry = new TabPage();
+            groupBox2 = new GroupBox();
             btnSave = new Button();
             calendar = new MonthCalendar();
             label9 = new Label();
             selDrawingTime = new ComboBox();
+            groupBox1 = new GroupBox();
             label11 = new Label();
             label10 = new Label();
             selExtraNumberRange = new ComboBox();
@@ -55,9 +57,15 @@
             label1 = new Label();
             selGameType = new ComboBox();
             tabCalculate = new TabPage();
+            button1 = new Button();
+            groupBox4 = new GroupBox();
+            listView1 = new ListView();
+            groupBox3 = new GroupBox();
             lstHistory = new ListView();
             tabPane.SuspendLayout();
             tabDataEntry.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numDataExtra).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numData6).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numData5).BeginInit();
@@ -66,6 +74,8 @@
             ((System.ComponentModel.ISupportInitialize)numData2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numData1).BeginInit();
             tabCalculate.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
             // tabPane
@@ -75,47 +85,38 @@
             tabPane.Location = new Point(12, 12);
             tabPane.Name = "tabPane";
             tabPane.SelectedIndex = 0;
-            tabPane.Size = new Size(865, 352);
+            tabPane.Size = new Size(808, 352);
             tabPane.TabIndex = 0;
             tabPane.SelectedIndexChanged += tabPane_SelectedIndexChanged;
             // 
             // tabDataEntry
             // 
-            tabDataEntry.Controls.Add(btnSave);
-            tabDataEntry.Controls.Add(calendar);
-            tabDataEntry.Controls.Add(label9);
-            tabDataEntry.Controls.Add(selDrawingTime);
-            tabDataEntry.Controls.Add(label11);
-            tabDataEntry.Controls.Add(label10);
-            tabDataEntry.Controls.Add(selExtraNumberRange);
-            tabDataEntry.Controls.Add(selNumbersRange);
-            tabDataEntry.Controls.Add(label8);
-            tabDataEntry.Controls.Add(label7);
-            tabDataEntry.Controls.Add(label6);
-            tabDataEntry.Controls.Add(label5);
-            tabDataEntry.Controls.Add(label4);
-            tabDataEntry.Controls.Add(label3);
-            tabDataEntry.Controls.Add(label2);
-            tabDataEntry.Controls.Add(numDataExtra);
-            tabDataEntry.Controls.Add(numData6);
-            tabDataEntry.Controls.Add(numData5);
-            tabDataEntry.Controls.Add(numData4);
-            tabDataEntry.Controls.Add(numData3);
-            tabDataEntry.Controls.Add(numData2);
-            tabDataEntry.Controls.Add(numData1);
-            tabDataEntry.Controls.Add(label1);
-            tabDataEntry.Controls.Add(selGameType);
+            tabDataEntry.Controls.Add(groupBox2);
+            tabDataEntry.Controls.Add(groupBox1);
             tabDataEntry.Location = new Point(4, 24);
             tabDataEntry.Name = "tabDataEntry";
             tabDataEntry.Padding = new Padding(3);
-            tabDataEntry.Size = new Size(857, 324);
+            tabDataEntry.Size = new Size(800, 324);
             tabDataEntry.TabIndex = 0;
             tabDataEntry.Text = "Data Entry";
             tabDataEntry.UseVisualStyleBackColor = true;
             // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnSave);
+            groupBox2.Controls.Add(calendar);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(selDrawingTime);
+            groupBox2.Location = new Point(522, 18);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(265, 303);
+            groupBox2.TabIndex = 35;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Action";
+            // 
             // btnSave
             // 
-            btnSave.Location = new Point(583, 269);
+            btnSave.Location = new Point(80, 250);
             btnSave.Name = "btnSave";
             btnSave.Size = new Size(75, 23);
             btnSave.TabIndex = 33;
@@ -125,14 +126,14 @@
             // 
             // calendar
             // 
-            calendar.Location = new Point(583, 95);
+            calendar.Location = new Point(19, 76);
             calendar.Name = "calendar";
             calendar.TabIndex = 32;
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(583, 33);
+            label9.Location = new Point(19, 14);
             label9.Name = "label9";
             label9.Size = new Size(86, 15);
             label9.TabIndex = 31;
@@ -141,15 +142,44 @@
             // selDrawingTime
             // 
             selDrawingTime.FormattingEnabled = true;
-            selDrawingTime.Location = new Point(583, 54);
+            selDrawingTime.Location = new Point(19, 33);
             selDrawingTime.Name = "selDrawingTime";
             selDrawingTime.Size = new Size(162, 23);
             selDrawingTime.TabIndex = 30;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(selExtraNumberRange);
+            groupBox1.Controls.Add(selNumbersRange);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(numDataExtra);
+            groupBox1.Controls.Add(numData6);
+            groupBox1.Controls.Add(numData5);
+            groupBox1.Controls.Add(numData4);
+            groupBox1.Controls.Add(numData3);
+            groupBox1.Controls.Add(numData2);
+            groupBox1.Controls.Add(numData1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(selGameType);
+            groupBox1.Location = new Point(6, 18);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(510, 303);
+            groupBox1.TabIndex = 34;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Data Input";
+            // 
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(228, 208);
+            label11.Location = new Point(275, 211);
             label11.Name = "label11";
             label11.Size = new Size(122, 15);
             label11.TabIndex = 29;
@@ -158,7 +188,7 @@
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(47, 208);
+            label10.Location = new Point(14, 211);
             label10.Name = "label10";
             label10.Size = new Size(98, 15);
             label10.TabIndex = 28;
@@ -167,23 +197,23 @@
             // selExtraNumberRange
             // 
             selExtraNumberRange.FormattingEnabled = true;
-            selExtraNumberRange.Location = new Point(228, 229);
+            selExtraNumberRange.Location = new Point(275, 232);
             selExtraNumberRange.Name = "selExtraNumberRange";
-            selExtraNumberRange.Size = new Size(162, 23);
+            selExtraNumberRange.Size = new Size(222, 23);
             selExtraNumberRange.TabIndex = 27;
             // 
             // selNumbersRange
             // 
             selNumbersRange.FormattingEnabled = true;
-            selNumbersRange.Location = new Point(47, 229);
+            selNumbersRange.Location = new Point(14, 232);
             selNumbersRange.Name = "selNumbersRange";
-            selNumbersRange.Size = new Size(162, 23);
+            selNumbersRange.Size = new Size(222, 23);
             selNumbersRange.TabIndex = 26;
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(467, 95);
+            label8.Location = new Point(434, 110);
             label8.Name = "label8";
             label8.Size = new Size(39, 15);
             label8.TabIndex = 24;
@@ -192,7 +222,7 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(397, 95);
+            label7.Location = new Point(364, 110);
             label7.Name = "label7";
             label7.Size = new Size(49, 15);
             label7.TabIndex = 23;
@@ -201,7 +231,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(327, 95);
+            label6.Location = new Point(294, 110);
             label6.Name = "label6";
             label6.Size = new Size(49, 15);
             label6.TabIndex = 25;
@@ -210,7 +240,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(257, 95);
+            label5.Location = new Point(224, 110);
             label5.Name = "label5";
             label5.Size = new Size(49, 15);
             label5.TabIndex = 22;
@@ -219,7 +249,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(187, 95);
+            label4.Location = new Point(154, 110);
             label4.Name = "label4";
             label4.Size = new Size(49, 15);
             label4.TabIndex = 21;
@@ -228,7 +258,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(117, 95);
+            label3.Location = new Point(84, 110);
             label3.Name = "label3";
             label3.Size = new Size(49, 15);
             label3.TabIndex = 20;
@@ -237,7 +267,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(47, 95);
+            label2.Location = new Point(14, 110);
             label2.Name = "label2";
             label2.Size = new Size(49, 15);
             label2.TabIndex = 19;
@@ -246,70 +276,84 @@
             // numDataExtra
             // 
             numDataExtra.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
-            numDataExtra.Location = new Point(466, 121);
+            numDataExtra.Location = new Point(433, 136);
+            numDataExtra.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numDataExtra.Name = "numDataExtra";
             numDataExtra.Size = new Size(64, 46);
             numDataExtra.TabIndex = 18;
             numDataExtra.TextAlign = HorizontalAlignment.Center;
+            numDataExtra.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // numData6
             // 
             numData6.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
-            numData6.Location = new Point(396, 121);
+            numData6.Location = new Point(363, 136);
+            numData6.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numData6.Name = "numData6";
             numData6.Size = new Size(64, 46);
             numData6.TabIndex = 17;
             numData6.TextAlign = HorizontalAlignment.Center;
+            numData6.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // numData5
             // 
             numData5.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
-            numData5.Location = new Point(326, 121);
+            numData5.Location = new Point(293, 136);
+            numData5.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numData5.Name = "numData5";
             numData5.Size = new Size(64, 46);
             numData5.TabIndex = 16;
             numData5.TextAlign = HorizontalAlignment.Center;
+            numData5.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // numData4
             // 
             numData4.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
-            numData4.Location = new Point(256, 121);
+            numData4.Location = new Point(223, 136);
+            numData4.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numData4.Name = "numData4";
             numData4.Size = new Size(64, 46);
             numData4.TabIndex = 15;
             numData4.TextAlign = HorizontalAlignment.Center;
+            numData4.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // numData3
             // 
             numData3.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
-            numData3.Location = new Point(186, 121);
+            numData3.Location = new Point(153, 136);
+            numData3.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numData3.Name = "numData3";
             numData3.Size = new Size(64, 46);
             numData3.TabIndex = 14;
             numData3.TextAlign = HorizontalAlignment.Center;
+            numData3.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // numData2
             // 
             numData2.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
-            numData2.Location = new Point(116, 121);
+            numData2.Location = new Point(83, 136);
+            numData2.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numData2.Name = "numData2";
             numData2.Size = new Size(64, 46);
             numData2.TabIndex = 13;
             numData2.TextAlign = HorizontalAlignment.Center;
+            numData2.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // numData1
             // 
             numData1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
-            numData1.Location = new Point(46, 121);
+            numData1.Location = new Point(13, 136);
+            numData1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numData1.Name = "numData1";
             numData1.Size = new Size(64, 46);
             numData1.TabIndex = 12;
             numData1.TextAlign = HorizontalAlignment.Center;
+            numData1.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(47, 33);
+            label1.Location = new Point(14, 36);
             label1.Name = "label1";
             label1.Size = new Size(71, 15);
             label1.TabIndex = 11;
@@ -318,27 +362,67 @@
             // selGameType
             // 
             selGameType.FormattingEnabled = true;
-            selGameType.Location = new Point(47, 54);
+            selGameType.Location = new Point(14, 57);
             selGameType.Name = "selGameType";
             selGameType.Size = new Size(162, 23);
             selGameType.TabIndex = 10;
+            selGameType.SelectedIndexChanged += selGameType_SelectedIndexChanged;
             // 
             // tabCalculate
             // 
-            tabCalculate.Controls.Add(lstHistory);
+            tabCalculate.Controls.Add(button1);
+            tabCalculate.Controls.Add(groupBox4);
+            tabCalculate.Controls.Add(groupBox3);
             tabCalculate.Location = new Point(4, 24);
             tabCalculate.Name = "tabCalculate";
             tabCalculate.Padding = new Padding(3);
-            tabCalculate.Size = new Size(857, 324);
+            tabCalculate.Size = new Size(800, 324);
             tabCalculate.TabIndex = 1;
             tabCalculate.Text = "Get Lucky";
             tabCalculate.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            button1.Location = new Point(664, 90);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Calculate";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(listView1);
+            groupBox4.Location = new Point(310, 16);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(289, 290);
+            groupBox4.TabIndex = 3;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Suggested Combinations";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(6, 23);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(277, 261);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(lstHistory);
+            groupBox3.Location = new Point(6, 16);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(289, 290);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Drawings History";
+            // 
             // lstHistory
             // 
-            lstHistory.Location = new Point(21, 37);
+            lstHistory.Location = new Point(6, 23);
             lstHistory.Name = "lstHistory";
-            lstHistory.Size = new Size(286, 261);
+            lstHistory.Size = new Size(277, 261);
             lstHistory.TabIndex = 0;
             lstHistory.UseCompatibleStateImageBehavior = false;
             // 
@@ -346,14 +430,17 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(889, 376);
+            ClientSize = new Size(832, 376);
             Controls.Add(tabPane);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             Name = "MainForm";
             Text = "SelectLuckyNumber";
             tabPane.ResumeLayout(false);
             tabDataEntry.ResumeLayout(false);
-            tabDataEntry.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)numDataExtra).EndInit();
             ((System.ComponentModel.ISupportInitialize)numData6).EndInit();
             ((System.ComponentModel.ISupportInitialize)numData5).EndInit();
@@ -362,6 +449,8 @@
             ((System.ComponentModel.ISupportInitialize)numData2).EndInit();
             ((System.ComponentModel.ISupportInitialize)numData1).EndInit();
             tabCalculate.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -395,5 +484,11 @@
         private ComboBox selGameType;
         private TabPage tabCalculate;
         private ListView lstHistory;
+        private GroupBox groupBox2;
+        private GroupBox groupBox1;
+        private Button button1;
+        private GroupBox groupBox4;
+        private ListView listView1;
+        private GroupBox groupBox3;
     }
 }
--- SelectLuckyNumber/SelectLuckyNumber/MainForm.cs
+++ SelectLuckyNumber/SelectLuckyNumber/MainForm.cs
@@ -12,6 +12,16 @@ namespace SelectLuckyNumber
         }
         private void RedrawControls()
         {
+            foreach (int value in Enumerable.Range(1, 100))
+            {
+                selNumbersRange.Items.Add(value);
+            }
+
+            foreach (int value in Enumerable.Range(1, 60))
+            {
+                selExtraNumberRange.Items.Add(value);
+            }
+
             foreach (string name in Enum.GetNames(typeof(GameType)))
             {
                 selGameType.Items.Add(name);
@@ -22,19 +32,8 @@ namespace SelectLuckyNumber
             {
                 selDrawingTime.Items.Add(name);
             }
-            selDrawingTime.SelectedIndex = (int)ExtraTime.Morning;
+            selDrawingTime.SelectedIndex = (int)ExtraTime.Night;
 
-            foreach (int value in Enumerable.Range(1, 100))
-            {
-                selNumbersRange.Items.Add(value);
-            }
-            selNumbersRange.SelectedIndex = 75;
-
-            foreach (int value in Enumerable.Range(1, 60))
-            {
-                selExtraNumberRange.Items.Add(value);
-            }
-            selExtraNumberRange.SelectedIndex = 35;
 
             lstHistory.View = View.Details;
             lstHistory.Columns.Add("Date Time", 70, HorizontalAlignment.Center);
@@ -44,6 +43,7 @@ namespace SelectLuckyNumber
         }
         private void btnSave_Click(object sender, EventArgs e)
         {
+            if (!isValid()) return;
             OneData newData = new OneData();
             int[] numbers = new int[6];
             numbers[0] = (int)numData1.Value;
@@ -70,7 +70,9 @@ namespace SelectLuckyNumber
         }
         private void readFromFile()
         {
-            if(!File.Exists(filePath)) { return; }
+            history.Clear();
+
+            if (!File.Exists(filePath)) { return; }
             string readText = File.ReadAllText(filePath);
 
             string[] datas = readText.Split('\n');
@@ -85,9 +87,11 @@ namespace SelectLuckyNumber
 
         private void tabPane_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(tabPane.SelectedIndex == 1)
+            if (tabPane.SelectedIndex == 1)
             {
                 readFromFile();
+
+                lstHistory.Items.Clear();
 
                 foreach (OneData one in history)
                 {
@@ -98,6 +102,118 @@ namespace SelectLuckyNumber
                     lstHistory.Items.Add(item);
                 }
             }
+        }
+
+        private void selGameType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch ((GameType)selGameType.SelectedIndex)
+            {
+                case GameType.Pick3:
+                    numData4.Enabled = false;
+                    numData5.Enabled = false;
+                    numData6.Enabled = false;
+                    numDataExtra.Enabled = false;
+                    selNumbersRange.SelectedIndex = 8;
+                    selExtraNumberRange.SelectedIndex = 8;
+                    selNumbersRange.Enabled = false;
+                    selExtraNumberRange.Enabled = false;
+                    selDrawingTime.Enabled = true;
+                    break;
+                case GameType.Pick4:
+                    numData4.Enabled = true;
+                    numData5.Enabled = false;
+                    numData6.Enabled = false;
+                    numDataExtra.Enabled = false;
+                    selNumbersRange.SelectedIndex = 8;
+                    selExtraNumberRange.SelectedIndex = 8;
+                    selNumbersRange.Enabled = false;
+                    selExtraNumberRange.Enabled = false;
+                    selDrawingTime.Enabled = true;
+                    break;
+                case GameType.Pick5:
+                    numData4.Enabled = true;
+                    numData5.Enabled = true;
+                    numData6.Enabled = false;
+                    numDataExtra.Enabled = false;
+                    selNumbersRange.SelectedIndex = 48;
+                    selExtraNumberRange.SelectedIndex = 8;
+                    selNumbersRange.Enabled = true;
+                    selExtraNumberRange.Enabled = false;
+                    selDrawingTime.Enabled = true;
+                    break;
+                case GameType.Pick6:
+                    numData4.Enabled = true;
+                    numData5.Enabled = true;
+                    numData6.Enabled = true;
+                    numDataExtra.Enabled = false;
+                    selNumbersRange.SelectedIndex = 45;
+                    selExtraNumberRange.SelectedIndex = 8;
+                    selNumbersRange.Enabled = false;
+                    selExtraNumberRange.Enabled = false;
+                    selDrawingTime.Enabled = true;
+                    break;
+                case GameType.MegaMillions:
+                    numData4.Enabled = true;
+                    numData5.Enabled = true;
+                    numData6.Enabled = false;
+                    numDataExtra.Enabled = true;
+                    selNumbersRange.SelectedIndex = 69;
+                    selExtraNumberRange.SelectedIndex = 24;
+                    selNumbersRange.Enabled = true;
+                    selExtraNumberRange.Enabled = true;
+                    selDrawingTime.Enabled = false;
+                    break;
+                case GameType.PowerBall:
+                    numData4.Enabled = true;
+                    numData5.Enabled = true;
+                    numData6.Enabled = false;
+                    numDataExtra.Enabled = true;
+                    selNumbersRange.SelectedIndex = 68;
+                    selExtraNumberRange.SelectedIndex = 25;
+                    selNumbersRange.Enabled = true;
+                    selExtraNumberRange.Enabled = true;
+                    selDrawingTime.Enabled = false;
+                    break;
+            }
+        }
+        private bool isValid()
+        {
+            if (numData1.Value > selNumbersRange.SelectedIndex + 1)
+            {
+                MessageBox.Show("Number 1 is Invalid.\n");
+                return false;
+            }
+            if (numData2.Value > selNumbersRange.SelectedIndex + 1)
+            {
+                MessageBox.Show("Number 2 is Invalid.\n");
+                return false;
+            }
+            if (numData3.Value > selNumbersRange.SelectedIndex + 1)
+            {
+                MessageBox.Show("Number 3 is Invalid.\n");
+                return false;
+            }
+            if (numData4.Value > selNumbersRange.SelectedIndex + 1)
+            {
+                MessageBox.Show("Number 4 is Invalid.\n");
+                return false;
+            }
+            if (numData5.Value > selNumbersRange.SelectedIndex + 1)
+            {
+                MessageBox.Show("Number 5 is Invalid.\n");
+                return false;
+            }
+            if (numData6.Value > selNumbersRange.SelectedIndex + 1)
+            {
+                MessageBox.Show("Number 6 is Invalid.\n");
+                return false;
+            }
+            if (numDataExtra.Value > selExtraNumberRange.SelectedIndex + 1)
+            {
+                MessageBox.Show("Extra Number must be less than the Extra Number Range.\n");
+                return false;
+            }
+            return true;
         }
     }
 }
